9b145b32c7a599a5a06af17c220fbd33cc48a0d1
Metallicow <metaliobovinus@gmail.com>
2018-04-14 12:11:19 -0500

    Initial commit

---
+++ importthis.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Bethesda Softworks, ZeniMax Online Studios, ZeniMax and their respective logos are registered trademarks of ZeniMax Media Incorporated.
+Attribution-ShareAlike 2.5 License
+All information supplied herein is provided freely with no guarantee of accuracy.
+"""
+
+BLACKSMITHING_ORE = ["Iron Ore", "High Iron Ore", "Orichalcum Ore", "Dwarven Ore", "Ebony Ore", "Calcinium Ore", "Galatite Ore", "Quicksilver Ore", "Voidstone Ore", "Rubedite Ore"]
+BLACKSMITHING_INGOT = ["Iron Ingot", "Steel Ingot", "Orichalcum Ingot", "Dwarven Ingot", "Ebony Ingot", "Calcinium Ingot", "Galatite Ingot", "Quicksilver Ingot", "Voidstone Ingot", "Rubedite Ingot"]
+BLACKSMITHING_TEMPER = ["Honing Stone", "Dwarven Oil", "Grain Solvent", "Tempering Alloy"]
+BLACKSMITHING_TEMPER_INFO = ["Improve quality from Normal to Fine", "Improve quality from Fine to Superior", "Improve quality from Superior to Epic", "Improve quality from Epic to Legendary"]
+
+GEM_WEAPON = ["Chysolite", "Amethyst", "Ruby", "Jade", "Turquoise", "Carnelian", "Fire Opal", "Citrine", "Potent Nirncrux"]
+GEM_WEAPON_TRAIT = ["Powered", "Charged", "Precise", "Infused", "Defending", "Training", "Sharpened", "Decisive", "Nirnhoned"]
+GEM_WEAPON_TRAIT_INFO = ["Increases healing done", "Increases chance to apply status effects", "Increases Weapon and Spell Critical values", "Increases weapon enchantment effects and reduces enchantment cooldown", "Increases Physical and Spell Resistance", "Increases experience gained from kills", "Increases Armor and Spell Penetration", "Chance when gaining Ultimate to gain 1 additional Ultimate", "Increases Damage of the weapon"]
+
